01e4888886e11fb12f3e6ecf6180bda947c356db
Tristan Chong <tristanchong@gmail.com>
2014-04-19 22:27:32 -0700

    route ascii to ascii_from_text

--- lib/lita/handlers/ascii_art.rb
+++ lib/lita/handlers/ascii_art.rb
@@ -3,6 +3,8 @@ require "artii"
 module Lita
   module Handlers
     class AsciiArt < Handler
+      route(/^ascii\s+(.*)/i, :ascii_from_text, command: true, help: {
+        'ascii TEXT' => 'Generate ASCII art from TEXT.'})
     end
 
     Lita.register_handler(AsciiArt)
