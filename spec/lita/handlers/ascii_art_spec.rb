c585c6f9d21509c5b9099ac73b4074934a1d43c0
Tristan Chong <tristanchong@gmail.com>
2014-04-20 00:18:43 -0700

    change to match single string with newlines

--- spec/lita/handlers/ascii_art_spec.rb
+++ spec/lita/handlers/ascii_art_spec.rb
@@ -6,13 +6,7 @@ describe Lita::Handlers::AsciiArt, lita_handler: true do
   describe "#ascii_from_text" do
     it "responds with ASCII art generated from the given text" do
       send_command "ascii FRIDAY"
-      expect(replies[0]).to match("  ______ _____  _____ _____      __     __")
-      expect(replies[1]).to match(" |  ____|  __ \|_   _|  __ \   /\\ \   / /")
-      expect(replies[2]).to match(" | |__  | |__) | | | | |  | | /  \\ \_/ / ")
-      expect(replies[3]).to match(" |  __| |  _  /  | | | |  | |/ /\ \\   /  ")
-      expect(replies[4]).to match(" | |    | | \ \ _| |_| |__| / ____ \| |   ")
-      expect(replies[5]).to match(" |_|    |_|  \_\_____|_____/_/    \_\_|   ")
-      expect(replies[6]).to match("                                          ")
+      expect(replies.first).to match("  _____ ____  ___ ____    _ __   __\n |  ___|  _ \\|_ _|  _ \\  / \\\\ \\ / /\n | |_  | |_) || || | | |/ _ \\\\ V / \n |  _| |  _ < | || |_| / ___ \\| |  \n |_|   |_| \\_\\___|____/_/   \\_\\_|  \n                                   ")
     end
   end
 end
