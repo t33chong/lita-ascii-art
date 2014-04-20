75821428b4f8a160763a21b9bf4be84f50d46505
Tristan Chong <tristanchong@gmail.com>
2014-04-19 23:27:10 -0700

    try font change

--- lib/lita/handlers/ascii_art.rb
+++ lib/lita/handlers/ascii_art.rb
@@ -6,7 +6,7 @@ module Lita
       route(/^ascii\s+(.*)/i, :ascii_from_text, command: true, help: {
         'ascii TEXT' => 'Generate ASCII art from TEXT.'})
 
-        @@art = Artii::Base.new :font => 'standard'
+        @@art = Artii::Base.new :font => 'big'
 
       def ascii_from_text(response)
         puts response.matches.first
