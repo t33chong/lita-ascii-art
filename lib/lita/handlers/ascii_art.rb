1a5e4805950f2d2e8a89b27e0eb1f93703755366
Tristan Chong <tristanchong@gmail.com>
2014-04-19 22:35:36 -0700

    first try at ascii_from_text method

--- lib/lita/handlers/ascii_art.rb
+++ lib/lita/handlers/ascii_art.rb
@@ -5,6 +5,12 @@ module Lita
     class AsciiArt < Handler
       route(/^ascii\s+(.*)/i, :ascii_from_text, command: true, help: {
         'ascii TEXT' => 'Generate ASCII art from TEXT.'})
+
+        @@art = Artii::Base.new :font => 'standard'
+
+      def ascii_from_text(response)
+        response.reply @@art.asciify(response.matches.first)
+      end
     end
 
     Lita.register_handler(AsciiArt)
