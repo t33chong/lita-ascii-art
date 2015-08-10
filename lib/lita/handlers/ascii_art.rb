94448f01c6b0b7c6e0464a9551870b06be190d0a
Tristan Chong <tristanchong@gmail.com>
2015-08-10 16:01:30 -0700

    lib: Clean up code

--- lib/lita/handlers/ascii_art.rb
+++ lib/lita/handlers/ascii_art.rb
@@ -1,19 +1,16 @@
-require "artii"
+require 'artii'
 
 module Lita
   module Handlers
     class AsciiArt < Handler
-      route(/^ascii\s+(.*)/i, :ascii_from_text, command: true, help: {
-        t("help.ascii_key") => t("help.ascii_value")})
+      route(/^ascii\s+(.*)/i, :ascii_from_text, command: true,
+            help: { t('help.ascii_key') => t('help.ascii_value') })
 
-      @@art = Artii::Base.new :font => 'standard'
+      @@art = Artii::Base.new font: 'standard'
 
       def ascii_from_text(response)
-        s = ''
-        response.matches.first.each do |c|
-          s += @@art.asciify(c)
-        end
-        response.reply render_template("lita-ascii-art", message: s)
+        message = response.matches.first.map { |x| @@art.asciify(x) }.join
+        response.reply render_template('lita-ascii-art', message: message)
       end
     end
 
