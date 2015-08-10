6ad60dcd4b0d7f886ce84821ff0fec87828ff183
Tristan Chong <tristanchong@gmail.com>
2015-08-10 14:54:36 -0700

    lib: Add template_root filepath

--- lib/lita-ascii-art.rb
+++ lib/lita-ascii-art.rb
@@ -5,3 +5,8 @@ Lita.load_locales Dir[File.expand_path(
 )]
 
 require "lita/handlers/ascii_art"
+
+Lita::Handlers::AsciiArt.template_root File.expand_path(
+  File.join("..", "..", "templates"),
+ __FILE__
+)
